4ddf421f9d76aa2e3968a29184f9760f2c02a250
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-08-29 16:09:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// src/App.tsx
+
+import './index.css';
+
+function App() {
+  return (
+    <div className="bg-sky-500 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-4">Welcome to My Vite React App!</h1>
+        <p className="text-gray-600">Enjoy using Vite, React, TypeScript, and Tailwind CSS.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
