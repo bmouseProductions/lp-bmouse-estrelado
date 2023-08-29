edd3d6ac1fd3b8c30ab8f72b23b8a505c1e85a55
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-08-29 16:31:38 -0300

    bg animado

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import './index.css';
 
 function App() {
   return (
-    <div className="bg-sky-500 min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold mb-4">Welcome to My Vite React App!</h1>
-        <p className="text-gray-600">Enjoy using Vite, React, TypeScript, and Tailwind CSS.</p>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className="bg-primary star-field">
+        <div className="layer"></div>
+        <div className="layer"></div>
+        <div className="layer"></div>
       </div>
+
     </div>
   );
 }
