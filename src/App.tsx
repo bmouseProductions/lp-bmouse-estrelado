c74c7b583b6d362b6da285e95dcda3382a32aa8c
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-08-29 16:46:33 -0300

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import './index.css';
 function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <div className="bg-primary star-field">
+      <div className="bg-primary star-field z-10">
         <div className="layer"></div>
         <div className="layer"></div>
         <div className="layer"></div>
       </div>
+      
+      <div className='relative z-50'>
 
+      </div>
     </div>
   );
 }
